24a2a0320149a5e171ff54241275de0fc7b3cc4e
tv.unimol <doslovno@gmail.com>
2017-01-12 15:21:37 +0300

    implements LoaderManager in NewsActivity

--- app/src/main/java/com/example/manifest/getnews/NewsActivity.java
+++ app/src/main/java/com/example/manifest/getnews/NewsActivity.java
@@ -1,13 +1,58 @@
 package com.example.manifest.getnews;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class NewsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<List<News>> {
+
+    private NewsAdapter mAdapter;
+    public static final String TAG = NewsActivity.class.getSimpleName();
+    //constant value for the news loader ID
+    private static final int NEWS_LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        ListView newsListView = (ListView) findViewById(R.id.activity_news);
+        //create a new adapter that takes an empty list of news as input
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+        newsListView.setAdapter(mAdapter);
+
+        //get a reference to the LoaderManager, in order to interact with loaders.
+        LoaderManager loaderManager = getLoaderManager();
+        //initialize the loader. Pass in the int ID constant defined above and pass in null
+        //for the bundle. Pass in this activity.
+        loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
+        return new NewsLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
+        //clear the adapter of previous news data
+        mAdapter.clear();
+
+        //if there is a valid list of News, then add them to the adapter's data set.
+        //this will trigger the ListView to update.
+        if (data != null && !data.isEmpty()) {
+            mAdapter.addAll(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        mAdapter.clear();
     }
 }
